714dd51856c772345b6ff5253d765e7a37cc498a
Urvashi-iPhone <lathiyaurvashi36@gmail.com>
2017-03-04 12:55:11 +0530

    Initial Commit

---
+++ MSETCL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MSETCL
+//
+//  Created by Tecksky Techonologies on 3/4/17.
+//  Copyright © 2017 Tecksky Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MSETCL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MSETCL
+//
+//  Created by Tecksky Techonologies on 3/4/17.
+//  Copyright © 2017 Tecksky Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
